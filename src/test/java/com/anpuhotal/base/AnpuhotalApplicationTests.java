4d2fa55369141dde4b32b5dd6c8bc506280c273d
anbarasu-tech <anbarasum081@gmail.com>
2025-09-24 17:37:22 +0530

    new project anpuhotal

---
+++ src/test/java/com/anpuhotal/base/AnpuhotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anpuhotal.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnpuhotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
